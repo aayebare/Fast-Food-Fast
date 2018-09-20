3eabf8f4b019725109fd74c948736eba6f40bd9a
asiimwemark <mugishasiimwe@gmail.com>
2018-09-20 10:29:21 +0300

    adding new tests and validations

--- api/app.py
+++ api/app.py
@@ -28,21 +28,23 @@ def orders():
         #Check for empty content field in order
         if 'content' not in post_order or post_order['content'].strip()=="": 
             return jsonify("No order made,please add order and try again"), 400 
-        #Check for empty price field in order
-        if 'price' not in post_order or post_order['price']==" ":
-            return jsonify("This order has no price, please verify the price and try again"), 400 
-        if type(post_order['price']) is not int:
-             return jsonify ("please add a valid unit price"), 400   
-        #create new order and add it to all_orders
-        new_order = {
-            "id":len(all_orders)+1,
-            "content":post_order["content"],
-            "price":post_order["price"],
-            "date":now.strftime("%Y-%m-%d"),
-            "completed":False
-        }
-        all_orders.append(new_order)
-        return jsonify(new_order), 201
+        #check for price in order    
+        if 'price' in post_order:
+            if type(post_order['price']) is int or type(post_order['price']) is float:
+                #create new order and add it to all_orders
+                new_order = {
+                    "id":len(all_orders)+1,
+                    "content":post_order["content"],
+                    "price":post_order["price"],
+                    "date":now.strftime("%Y-%m-%d"),
+                    "completed":False
+                }
+                all_orders.append(new_order)
+                return jsonify(new_order), 201
+            else:    
+                return jsonify ("please add a valid unit price"), 400  
+        else:
+            return jsonify("This order has no price, please verify the price and try again"), 400
 
 @app.route('/api/v1/orders/<int:order_id>', methods=['GET', 'PUT'])
 def single_order(order_id):
@@ -55,14 +57,17 @@ def single_order(order_id):
     #Updating the status of an order
     else:
         input_order = request.get_json()
-        if 'completed' not in input_order:
+        if 'completed' in input_order:
+            #Check if status is of type boolean
+            if type(input_order["completed"]) is bool:
+                #update status of order
+                get_order[0]["completed"] = input_order["completed"]
+                return jsonify(get_order[0]), 200
+            else:  
+                return jsonify("please add a boolean value to change the status"), 400
+        else:
             return jsonify("please add a new status"), 400
-        #Check if status is of type boolean
-        if type(input_order["completed"]) is not bool:
-            return jsonify("please add a boolean value to change the status"), 400
-        #Check if order already completed    
-        if get_order[0]['completed'] and input_order['completed']==True:
-            return jsonify("order already completed"), 200
-        #update status of order
-        get_order[0]["completed"] = input_order["completed"]
-        return jsonify(get_order[0]), 200
+         
+        
+        
+       
--- test_endpoints.py
+++ test_endpoints.py
@@ -15,7 +15,8 @@ class TestApiEndpoints(unittest.TestCase):
 			'date': '12-9-18',
 			"completed":False
 			}
-			)
+			)	
+		
 	def test_if_all_orders_returned(self):
 		response = self.tester.get('/api/v1/orders')
 		self.assertEqual(response.status_code, 200)
@@ -39,7 +40,6 @@ class TestApiEndpoints(unittest.TestCase):
 		{
 			"id":1,
 			"content":"Chicken",
-			"price":" ",
 			'date': '5-6-8',
 			"completed":False
 			}
@@ -48,7 +48,7 @@ class TestApiEndpoints(unittest.TestCase):
 		self.assertEqual(response.status_code, 400)	
 		self.assertIn(b"This order has no price, please verify the price and try again", response.data)
 
-	def test_for_error_if_type_price_not_interger(self):
+	def test_for_error_if_type_price_not_interger_or_float(self):
 		dummy_data = json.dumps(
 		{
 		"content":"Beef",
@@ -59,25 +59,12 @@ class TestApiEndpoints(unittest.TestCase):
 		self.assertEqual(response.status_code, 400)	
 		self.assertIn(b"please add a valid unit price", response.data)	
 
-	def test_for_error_when_no_content_in_keys(self):	
-		dummy_data = json.dumps(
-			{
-			"id":1,
-			"price":100,
-			'date': '5-6-8',
-			"completed":False
-			}
-		)	
-		response = self.tester.post('/api/v1/orders', data=dummy_data, content_type = 'application/json')
-		self.assertEqual(response.status_code, 400)	
-		self.assertIn(b"No order made,please add order and try again", response.data)
-
 	def test_if_single_order_returned(self):
 		response = self.tester.get('/api/v1/orders/1')
 		self.assertEqual(response.status_code, 200)	
 
 	def test_for_error_if_order_id_not_in_all_orders(self):
-		response = self.tester.get('/api/v1/orders/8')
+		response = self.tester.get('/api/v1/orders/87')
 		self.assertEqual(response.status_code, 404)	
 		self.assertIn(b"no order with the given id, please try again", response.data)
 
